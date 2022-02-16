f0b9bb8f0a53edbaf07aa1b409edad963a18adf7
Alireza Ghias <a.ghias@natix.io>
2022-02-16 23:36:15 +0100

    json rpc setup

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate diesel;
 
 mod db;
 
+use chrono::format::Item::Error;
 use diesel::prelude::*;
 use uuid::Uuid;
 
@@ -14,11 +15,24 @@ use db::dao::dao_transaction;
 use schema::t_transaction::dsl;
 use crate::core::{CoreImpl, CoreAddress};
 use jsonrpc_http_server::jsonrpc_core::{IoHandler, Value, Params};
-use jsonrpc_http_server::ServerBuilder;
+use jsonrpc_http_server::{RequestMiddleware, RequestMiddlewareAction, Response, ServerBuilder};
+use jsonrpc_http_server::hyper::{Body, Request};
 
 
 use crate::core::address::Address;
 
+// struct Security;
+// impl RequestMiddleware for Security {
+//    fn on_request(&self, request: Request<Body>) -> RequestMiddlewareAction {
+//       let headers = request.headers();
+//       let auth = headers.get("Authorization");
+//       if auth.is_none() {
+//          RequestMiddlewareAction::from(Response::service_unavailable("Authentication Failed"))
+//       } else {
+//          RequestMiddlewareAction::from(Response::service_unavailable("Authentication Failed"))
+//       }
+//    }
+// }
 
 fn main() {
 
@@ -26,6 +40,7 @@ fn main() {
    io.extend_with(CoreImpl.to_delegate());
 
    let server = ServerBuilder::new(io)
+       // .request_middleware(Security)
        .threads(3)
        .start_http(&"127.0.0.1:3030".parse().unwrap())
        .unwrap();
